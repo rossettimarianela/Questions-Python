5e74c24dde21f4a5c37a965a777dfcf187262eb7
Marianela Rossetti <rossettimarianela@gmail.com>
2026-03-19 14:33:36 -0300

    Implementación de sistema de puntos

--- questions.py
+++ questions.py
@@ -15,7 +15,9 @@ words = [
 word = random.choice(words)
 guessed = []
 attempts = 6
+score = 0
 
+print()
 print("¡Bienvenido al Ahorcado!")
 print()
 
@@ -32,6 +34,7 @@ while attempts > 0:
     # Verificar si el jugador ya adivinó la palabra completa
     if "_" not in progress:
         print("¡Ganaste!")
+        score += 6
         break
 
     print(f"Intentos restantes: {attempts}")
@@ -50,14 +53,24 @@ while attempts > 0:
     else:
         guessed.append(letter)
         attempts -= 1
+        score -= 1
         print("Esa letra no está en la palabra.")
     print()
 
 else:
     print(f"¡Perdiste! La palabra era: {word}")
+    score = 0
+
+print(f'Puntaje Obtenido: {score}')
+
 
 
 '''El juego tiene un bug. Si el usuario ingresa más de una letra, un número o cualquier
 otro carácter inválido, el programa se comporta de manera inesperada.
 Modificalo para que en ese caso muestre el mensaje "Entrada no válida" y continúe el juego 
 en la siguiente iteración '''
+
+
+'''Modificá el juego para que al final de la partida se muestre el puntaje del jugador. 
+El puntaje se calcula de la siguiente forma: cada letra incorrecta resta 1 punto,
+adivinar la palabra completa suma 6 puntos, y perder deja el puntaje en 0.'''
